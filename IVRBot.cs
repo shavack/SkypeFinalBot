c64a6ce31a482fb4308b3e4803ffa9278e064c9b
shavack <dkozuch@gmail.com>
2017-12-05 10:02:16 +0100

    Initial commit

---
+++ IVRBot.cs
@@ -0,0 +1,187 @@
+﻿using System.Runtime.InteropServices;
+
+namespace EmergencyServicesBot
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Calling;
+    using Microsoft.Bot.Builder.Calling.Events;
+    using Microsoft.Bot.Builder.Calling.ObjectModel.Contracts;
+    using Microsoft.Bot.Builder.Calling.ObjectModel.Misc;
+
+    public class IVRBot : IDisposable, ICallingBot
+    {
+        private readonly MicrosoftCognitiveSpeechService speechService = new MicrosoftCognitiveSpeechService();
+        private int i;
+        private IncomingCallEvent incomingCallEvent;
+
+        public enum Intent
+        {
+            WelcomeMessage = 0,
+            DocQuery = 1,
+            CarQuery = 2,
+            CarSpecification = 3,
+            DocSpecification = 4,
+        }
+
+        public IVRBot(ICallingBotService callingBotService)
+        {
+            this.CallingBotService = callingBotService;
+
+            this.CallingBotService.OnIncomingCallReceived += this.OnIncomingCallReceived;
+            this.CallingBotService.OnRecordCompleted += this.OnRecordCompleted;
+            this.CallingBotService.OnHangupCompleted += OnHangupCompleted;
+        }
+
+        public ICallingBotService CallingBotService { get; }
+
+        public void Dispose()
+        {
+            if (this.CallingBotService != null)
+            {
+                this.CallingBotService.OnIncomingCallReceived -= this.OnIncomingCallReceived;
+                this.CallingBotService.OnRecordCompleted -= this.OnRecordCompleted;
+                this.CallingBotService.OnHangupCompleted -= OnHangupCompleted;
+            }
+        }
+
+        private static Task OnHangupCompleted(HangupOutcomeEvent hangupOutcomeEvent)
+        {
+            hangupOutcomeEvent.ResultingWorkflow = null;
+            return Task.FromResult(true);
+        }
+
+        private Task OnIncomingCallReceived(IncomingCallEvent incomingCallEvent)
+        {
+
+            var record = CreateRecordingAction("Hello! It's mega ultra super B A D Sparrows bot! How may I help you?", Intent.WelcomeMessage);
+
+            this.incomingCallEvent = incomingCallEvent;
+            this.incomingCallEvent.ResultingWorkflow.Actions = new List<ActionBase> {
+                new Answer { OperationId = Guid.NewGuid().ToString()},
+                record,
+            };
+
+
+            return Task.FromResult(true);
+        }
+
+        private ActionBase CreateRecordingAction(string text, Intent intent)
+        {
+            return new Record
+            {
+                OperationId = ((int)intent).ToString(),
+                PlayPrompt = new PlayPrompt { OperationId = Guid.NewGuid().ToString(), Prompts = new List<Prompt> { new Prompt { Value = text } } },
+                RecordingFormat = RecordingFormat.Wav,
+                MaxDurationInSeconds = 2,
+                PlayBeep = false,
+            };
+        }
+
+        private ActionBase CreatePlayPromptAction(string text)
+        {
+            return new PlayPrompt
+            {
+                OperationId = Guid.NewGuid().ToString(),
+                Prompts = new List<Prompt> { new Prompt { Value = text } }
+            };
+        }
+
+        private async Task OnRecordCompleted(RecordOutcomeEvent recordOutcomeEvent)
+        {
+            var actions = new List<ActionBase>();
+
+            var spokenText = string.Empty;
+
+            try
+            {
+                if (recordOutcomeEvent.RecordOutcome.Outcome == Outcome.Success)
+                {
+                    var record = await recordOutcomeEvent.RecordedContent;
+                    spokenText = await this.speechService.GetTextFromAudioAsync(record);
+
+                    switch (int.Parse(recordOutcomeEvent.RecordOutcome.Id))
+                    {
+                        case (int)Intent.WelcomeMessage:
+                            if (spokenText.ToUpperInvariant().Contains("CAR"))
+                            {
+                                actions.Add(CreateRecordingAction("what kind of car do you want?", Intent.CarQuery));
+                            }
+                            else if (spokenText.ToUpperInvariant().Contains("DOCUMENT") &&
+                                     !spokenText.ToUpperInvariant().Contains("CAR"))
+
+                            {
+                                actions.Add(CreateRecordingAction("what kind of docs do you want to send?", Intent.DocQuery));
+                            }
+                            else
+                            {
+                                actions.Add(CreateRecordingAction(
+                                    $"We couldn't recognize your message. Did you say {spokenText}? You should choose either Documents or Car. Please repeat.", Intent.WelcomeMessage));
+                            }
+                            break;
+                        case (int)Intent.CarQuery:
+                            if (spokenText.ToUpperInvariant().Contains("MUSTANG"))
+                            {
+                                actions.Add(CreatePlayPromptAction("Your mustang will arrive soon. Thank you for using our service."));
+                                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                            }
+                            else if (spokenText.ToUpperInvariant().Contains("FERRARI"))
+                            {
+                                actions.Add(CreatePlayPromptAction("Your Ferrari will arrive soon. Thank you for using our service."));
+                                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                            }
+                            else
+                            {
+                                actions.Add(CreatePlayPromptAction(
+                                    "We weren't able to understand your request (you didn't choose Mustang or Ferrari), so you will get a fiat multipla. see ya!"));
+                                actions.Add(new Hangup {OperationId = Guid.NewGuid().ToString()});
+                            }
+                            break;
+                        case (int)Intent.DocQuery:
+                            if (spokenText.ToUpperInvariant().Contains("CREDIT CARD"))
+                            {
+                                actions.Add(CreatePlayPromptAction("Your credit card info will be sent soon. Thank you for using our service."));
+                                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                            }
+                            else if (spokenText.ToUpperInvariant().Contains("DRIVING LICENCE"))
+                            {
+                                actions.Add(CreatePlayPromptAction("Your driving licence info will be sent soon. Thank you for using our service."));
+                                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                            }
+                            else
+                            {
+                                actions.Add(CreatePlayPromptAction("We weren't able to understand your request (you didn't choose Credit card or Driving licence, so you will get some spam. See ya!"));
+                                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                            }
+                            break;
+                        case (int)Intent.CarSpecification:
+                            break;
+                        case (int)Intent.DocSpecification:
+                            break;
+                    }
+                }
+            else
+            {
+                actions.Add(CreatePlayPromptAction("Sorry, there was an issue."));
+            }
+
+            //actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() }); // hang up the call
+            recordOutcomeEvent.ResultingWorkflow.Actions = actions;
+            this.incomingCallEvent.ResultingWorkflow.Actions = actions;
+            }
+            catch (Exception e)
+            {
+                var action = CreatePlayPromptAction(e.ToString());
+                actions.Add(action);
+                actions.Add(new Hangup { OperationId = Guid.NewGuid().ToString() });
+                recordOutcomeEvent.ResultingWorkflow.Actions = actions;
+                this.incomingCallEvent.ResultingWorkflow.Actions = actions;
+            }
+        }
+    }
+}
---
+++ Services/MSCognitiveSpeechService.cs
@@ -0,0 +1,95 @@
+﻿using System.Web;
+
+namespace EmergencyServicesBot
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using System.Web.Configuration;
+    using Microsoft.Bing.Speech;
+    using Newtonsoft.Json;
+
+    public class MicrosoftCognitiveSpeechService
+    {
+        private readonly string subscriptionKey;
+        private readonly string speechRecognitionUri;
+
+        public MicrosoftCognitiveSpeechService()
+        {
+            this.DefaultLocale = "en-US";
+            this.subscriptionKey = WebConfigurationManager.AppSettings["MicrosoftSpeechApiKey"];
+            this.speechRecognitionUri = Uri.UnescapeDataString(WebConfigurationManager.AppSettings["MicrosoftSpeechRecognitionUri"]);
+
+        }
+
+        public string DefaultLocale { get; set; }
+
+        /// <summary>
+        /// Gets text from an audio stream.
+        /// </summary>
+        /// <param name="audiostream"></param>
+        /// <returns>Transcribed text. </returns>
+        public async Task<string> GetTextFromAudioAsync(Stream audiostream)
+        {
+            var requestUri = speechRecognitionUri + Guid.NewGuid();
+
+            using (var client = new HttpClient())
+            {
+                var token = Authentication.Instance.GetAccessToken();
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                try
+                {
+                    using (var binaryContent = new ByteArrayContent(StreamToBytes(audiostream)))
+                    {
+                        binaryContent.Headers.TryAddWithoutValidation("content-type", "audio/wav; codec=\"audio/pcm\"; samplerate=16000");
+                        var response = await client.PostAsync(requestUri, binaryContent);
+                        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                        {
+                            throw new HttpException((int)response.StatusCode, $"({response.StatusCode}) {response.ReasonPhrase}");
+                        }
+                        var responseString = await response.Content.ReadAsStringAsync();
+                        if (string.IsNullOrEmpty(responseString))
+                        {
+                            return "Returned string was empty";
+                        }
+
+                        dynamic data = JsonConvert.DeserializeObject(responseString);
+                        
+                        if (data != null)
+                        {
+                            return data["DisplayText"] ?? "REPEAT";
+                        }
+                        else
+                        {
+                            return string.Empty;
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Debug.WriteLine(exp);
+                    return $"issue with cognitive speech service: {exp}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts Stream into byte[].
+        /// </summary>
+        /// <param name="input">Input stream</param>
+        /// <returns>Output byte[]</returns>
+        private static byte[] StreamToBytes(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
